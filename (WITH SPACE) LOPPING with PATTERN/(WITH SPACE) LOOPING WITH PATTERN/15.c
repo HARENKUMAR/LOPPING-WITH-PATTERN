9bf8acd00c90f8c7f832ec2f0de93ac4fd7fcd65
HARENKUMAR <dhamiharenkumar@gmail.com>
2024-10-08 20:02:00 +0530

    Add files via upload

---
+++ (WITH SPACE) LOPPING with PATTERN/(WITH SPACE) LOOPING WITH PATTERN/15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	char  i,j,s;
+	
+	for(i='E'; i>='A'; i--)
+	{
+		for(s='E'; s>i; s--)
+		{
+			printf(" ");
+		}
+		for(j='A'; j<=i; j++)
+		{
+			printf("%c",j);
+		}
+	printf("\n");	
+	}
+}
